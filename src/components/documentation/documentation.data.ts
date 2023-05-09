8fbbce938753d2e20a5e7245cdcf3128e2efa066
SkynerSany <skynerstudent@gmail.com>
2023-05-09 15:46:14 +0300

    feat: add navigation and data saving

--- src/components/documentation/documentation.data.ts
+++ src/components/documentation/documentation.data.ts
@@ -1,4 +1,4 @@
-import { IRoot } from './documentation.interfaces';
+import { IRoot, TAllNodes } from './documentation.interfaces';
 
 class Info {
   fields = {
@@ -7,36 +7,105 @@ class Info {
     pages: 'Int',
     prev: 'Int',
   };
+  checked = {
+    count: false,
+    next: false,
+    pages: false,
+    prev: false,
+  };
+  fieldsType = {
+    count: 'Int',
+    next: 'Int',
+    pages: 'Int',
+    prev: 'Int',
+  };
+
+  parent: TAllNodes;
+  constructor(parent: TAllNodes) {
+    this.parent = parent;
+  }
 }
 
 class Character {
   arguments = {
     id: 'ID',
   };
+  checked = {
+    created: false,
+    episode: false,
+    gender: false,
+    id: false,
+    image: false,
+    location: false,
+    name: false,
+    origin: false,
+    species: false,
+    status: false,
+    type: false,
+  };
   fields = {
     created: 'String',
-    episode: () => new Episode(),
+    episode: () => new Episode(this),
     gender: 'String',
     id: 'ID',
     image: 'String',
-    location: () => new Location(),
+    location: () => new Location(this),
     name: 'String',
-    origin: () => new Location(),
+    origin: () => new Location(this),
     species: 'String',
     status: 'String',
     type: 'String',
   };
+  fieldsType = {
+    created: 'String',
+    episode: 'Episode',
+    gender: 'String',
+    id: 'ID',
+    image: 'String',
+    location: 'Location',
+    name: 'String',
+    origin: 'Location',
+    species: 'String',
+    status: 'String',
+    type: 'String',
+  };
+
+  parent: TAllNodes;
+  constructor(parent: TAllNodes) {
+    this.parent = parent;
+  }
 }
 
 class Episode {
   fields = {
     air_date: 'String',
-    characters: () => new Character(),
+    characters: () => new Character(this),
     created: 'String',
     episode: 'String',
     id: 'ID',
     name: 'String',
   };
+  checked = {
+    air_date: false,
+    characters: false,
+    created: false,
+    episode: false,
+    id: false,
+    name: false,
+  };
+  fieldsType = {
+    air_date: 'String',
+    characters: 'Character',
+    created: 'String',
+    episode: 'String',
+    id: 'ID',
+    name: 'String',
+  };
+
+  parent: TAllNodes;
+  constructor(parent: TAllNodes) {
+    this.parent = parent;
+  }
 }
 
 class Location {
@@ -45,9 +114,30 @@ class Location {
     dimension: 'String',
     id: 'ID',
     name: 'String',
-    residents: () => new Character(),
+    residents: () => new Character(this),
     type: 'String',
   };
+  checked = {
+    created: false,
+    dimension: false,
+    id: false,
+    name: false,
+    residents: false,
+    type: false,
+  };
+  fieldsType = {
+    created: 'String',
+    dimension: 'String',
+    id: 'ID',
+    name: 'String',
+    residents: 'Character',
+    type: 'String',
+  };
+
+  parent: TAllNodes;
+  constructor(parent: TAllNodes) {
+    this.parent = parent;
+  }
 }
 
 class FilterEpisode {
@@ -55,17 +145,43 @@ class FilterEpisode {
     episode: 'String',
     name: 'String',
   };
+  checked = {
+    episode: false,
+    name: false,
+  };
+  fieldsType = {
+    episode: 'String',
+    name: 'String',
+  };
+
+  parent: TAllNodes;
+  constructor(parent: TAllNodes) {
+    this.parent = parent;
+  }
 }
 
 class Episodes {
   arguments = {
-    filter: () => new FilterEpisode(),
+    filter: () => new FilterEpisode(this),
     page: 'Int',
   };
   fields = {
-    info: () => new Info(),
-    results: () => new Episode(),
+    info: () => new Info(this),
+    results: () => new Episode(this),
   };
+  checked = {
+    info: false,
+    results: false,
+  };
+  fieldsType = {
+    info: 'Info',
+    results: 'Episode',
+  };
+
+  parent: TAllNodes;
+  constructor(parent: TAllNodes) {
+    this.parent = parent;
+  }
 }
 
 class EpisodesByIds {
@@ -74,12 +190,33 @@ class EpisodesByIds {
   };
   fields = {
     air_date: 'String',
-    characters: () => new Character(),
+    characters: () => new Character(this),
     created: 'String',
     episode: 'String',
     id: 'ID',
     name: 'String',
   };
+  checked = {
+    air_date: false,
+    characters: false,
+    created: false,
+    episode: false,
+    id: false,
+    name: false,
+  };
+  fieldsType = {
+    air_date: 'String',
+    characters: 'Character',
+    created: 'String',
+    episode: 'String',
+    id: 'ID',
+    name: 'String',
+  };
+
+  parent: TAllNodes;
+  constructor(parent: TAllNodes) {
+    this.parent = parent;
+  }
 }
 
 class FilterLocation {
@@ -88,17 +225,45 @@ class FilterLocation {
     name: 'String',
     type: 'String',
   };
+  checked = {
+    dimension: false,
+    name: false,
+    type: false,
+  };
+  fieldsType = {
+    dimension: 'String',
+    name: 'String',
+    type: 'String',
+  };
+
+  parent: TAllNodes;
+  constructor(parent: TAllNodes) {
+    this.parent = parent;
+  }
 }
 
 class Locations {
   arguments = {
-    filter: () => new FilterLocation(),
+    filter: () => new FilterLocation(this),
     page: 'Int',
   };
   fields = {
-    info: () => new Info(),
-    results: () => new Location(),
+    info: () => new Info(this),
+    results: () => new Location(this),
   };
+  checked = {
+    info: false,
+    results: false,
+  };
+  fieldsType = {
+    info: () => 'Info',
+    results: () => 'Location',
+  };
+
+  parent: TAllNodes;
+  constructor(parent: TAllNodes) {
+    this.parent = parent;
+  }
 }
 
 class LocationsByIds {
@@ -110,9 +275,30 @@ class LocationsByIds {
     dimension: 'String',
     id: 'ID',
     name: 'String',
-    residents: () => new Character(),
+    residents: () => new Character(this),
     type: 'String',
   };
+  checked = {
+    created: false,
+    dimension: false,
+    id: false,
+    name: false,
+    residents: false,
+    type: false,
+  };
+  fieldsType = {
+    created: 'String',
+    dimension: 'String',
+    id: 'ID',
+    name: 'String',
+    residents: 'Character',
+    type: 'String',
+  };
+
+  parent: TAllNodes;
+  constructor(parent: TAllNodes) {
+    this.parent = parent;
+  }
 }
 
 class FilterCharacter {
@@ -123,17 +309,49 @@ class FilterCharacter {
     status: 'String',
     type: 'String',
   };
+  checked = {
+    gender: false,
+    name: false,
+    species: false,
+    status: false,
+    type: false,
+  };
+  fieldsType = {
+    gender: 'String',
+    name: 'String',
+    species: 'String',
+    status: 'String',
+    type: 'String',
+  };
+
+  parent: TAllNodes;
+  constructor(parent: TAllNodes) {
+    this.parent = parent;
+  }
 }
 
 class Characters {
   arguments = {
-    filter: () => new FilterCharacter(),
+    filter: () => new FilterCharacter(this),
     page: 'Int',
   };
   fields = {
-    info: () => new Info(),
-    results: () => new Character(),
+    info: () => new Info(this),
+    results: () => new Character(this),
   };
+  checked = {
+    info: false,
+    results: false,
+  };
+  fieldsType = {
+    info: 'Info',
+    results: 'Info',
+  };
+
+  parent: TAllNodes;
+  constructor(parent: TAllNodes) {
+    this.parent = parent;
+  }
 }
 
 class CharactersByIds {
@@ -142,39 +360,103 @@ class CharactersByIds {
   };
   fields = {
     created: 'String',
-    episode: () => new Episode(),
+    episode: () => new Episode(this),
     gender: 'String',
     id: 'ID',
     image: 'String',
-    location: () => new Location(),
+    location: () => new Location(this),
     name: 'String',
-    origin: () => new Location(),
+    origin: () => new Location(this),
     species: 'String',
     status: 'String',
     type: 'String',
   };
+  checked = {
+    created: false,
+    episode: false,
+    gender: false,
+    id: false,
+    image: false,
+    location: false,
+    name: false,
+    origin: false,
+    species: false,
+    status: false,
+    type: false,
+  };
+  fieldsType = {
+    created: 'String',
+    episode: 'Episode',
+    gender: 'String',
+    id: 'ID',
+    image: 'String',
+    location: 'Location',
+    name: 'String',
+    origin: 'Location',
+    species: 'String',
+    status: 'String',
+    type: 'String',
+  };
+
+  parent: TAllNodes;
+  constructor(parent: TAllNodes) {
+    this.parent = parent;
+  }
 }
 
 class Query {
   fields = {
-    character: () => new Character(),
-    characters: () => new Characters(),
-    charactersByIds: () => new CharactersByIds(),
-    episode: () => new Episode(),
-    episodes: () => new Episodes(),
-    episodesByIds: () => new EpisodesByIds(),
-    location: () => new Location(),
-    locations: () => new Locations(),
-    locationsByIds: () => new LocationsByIds(),
+    character: () => new Character(this),
+    characters: () => new Characters(this),
+    charactersByIds: () => new CharactersByIds(this),
+    episode: () => new Episode(this),
+    episodes: () => new Episodes(this),
+    episodesByIds: () => new EpisodesByIds(this),
+    location: () => new Location(this),
+    locations: () => new Locations(this),
+    locationsByIds: () => new LocationsByIds(this),
   };
+  checked = {
+    character: false,
+    characters: false,
+    charactersByIds: false,
+    episode: false,
+    episodes: false,
+    episodesByIds: false,
+    location: false,
+    locations: false,
+    locationsByIds: false,
+  };
+  fieldsType = {
+    character: 'Character',
+    characters: 'Characters',
+    charactersByIds: 'CharactersByIds',
+    episode: 'Episode',
+    episodes: 'Episodes',
+    episodesByIds: 'EpisodesByIds',
+    location: 'Location',
+    locations: 'Locations',
+    locationsByIds: 'LocationsByIds',
+  };
+
+  parent: TAllNodes;
+  constructor(parent: TAllNodes) {
+    this.parent = parent;
+  }
 }
 
 class Root {
   fields = {
-    query: () => new Query(),
+    query: () => new Query(this),
+  };
+  checked = {
+    query: false,
+  };
+  fieldsType = {
+    query: 'Query',
   };
 }
 
-export function getData(): IRoot {
+export function getDocumentationInitialData(): IRoot {
   return new Root();
 }
--- src/components/documentation/documentation.interfaces.ts
+++ src/components/documentation/documentation.interfaces.ts
@@ -1,174 +1,33 @@
-interface IInfo {
-  fields: {
-    count: string;
-    next: string;
-    pages: string;
-    prev: string;
-  };
-}
-
-interface ICharacter {
-  arguments: {
-    id: string;
-  };
-  fields: {
-    created: string;
-    episode: () => IEpisode;
-    gender: string;
-    id: string;
-    image: string;
-    location: () => ILocation;
-    name: string;
-    origin: () => ILocation;
-    species: string;
-    status: string;
-    type: string;
-  };
-}
-
-interface IEpisode {
-  fields: {
-    air_date: string;
-    characters: () => ICharacter;
-    created: string;
-    episode: string;
-    id: string;
-    name: string;
-  };
-}
-
-interface ILocation {
-  fields: {
-    created: string;
-    dimension: string;
-    id: string;
-    name: string;
-    residents: () => ICharacter;
-    type: string;
-  };
-}
-
-interface IFilterEpisode {
-  fields: {
-    episode: string;
-    name: string;
-  };
-}
-
-interface IEpisodes {
-  arguments: {
-    filter: () => IFilterEpisode;
-    page: string;
-  };
-  fields: {
-    info: () => IInfo;
-    results: () => IEpisode;
-  };
-}
-
-interface IEpisodesByIds {
-  arguments: {
-    ids: string;
-  };
-  fields: {
-    air_date: string;
-    characters: () => ICharacter;
-    created: string;
-    episode: string;
-    id: string;
-    name: string;
-  };
-}
-
-interface IFilterLocation {
-  fields: {
-    dimension: string;
-    name: string;
-    type: string;
-  };
-}
-
-interface ILocations {
-  arguments: {
-    filter: () => IFilterLocation;
-    page: string;
-  };
-  fields: {
-    info: () => IInfo;
-    results: () => ILocation;
-  };
-}
-
-interface ILocationsByIds {
-  arguments: {
-    ids: string;
-  };
-  fields: {
-    created: string;
-    dimension: string;
-    id: string;
-    name: string;
-    residents: () => ICharacter;
-    type: string;
-  };
-}
-
-interface IFilterCharacter {
-  fields: {
-    gender: string;
-    name: string;
-    species: string;
-    status: string;
-    type: string;
-  };
-}
-
-interface ICharacters {
-  arguments: {
-    filter: () => IFilterCharacter;
-    page: string;
-  };
-  fields: {
-    info: () => IInfo;
-    results: () => ICharacter;
-  };
-}
-
-interface ICharactersByIds {
-  arguments: {
-    ids: string;
-  };
-  fields: {
-    created: string;
-    episode: () => IEpisode;
-    gender: string;
-    id: string;
-    image: string;
-    location: () => ILocation;
-    name: string;
-    origin: () => ILocation;
-    species: string;
-    status: string;
-    type: string;
-  };
-}
-
-interface IQuery {
-  fields: {
-    character: () => ICharacter;
-    characters: () => ICharacters;
-    charactersByIds: () => ICharactersByIds;
-    episode: () => IEpisode;
-    episodes: () => IEpisodes;
-    episodesByIds: () => IEpisodesByIds;
-    location: () => ILocation;
-    locations: () => ILocations;
-    locationsByIds: () => ILocationsByIds;
-  };
-}
-
+export type TAllNodes = IRoot | object;
+export type TRootFunction = () => TAllNodes;
 export interface IRoot {
   fields: {
-    query: () => IQuery;
+    [key: string]: TRootFunction | TAllNodes;
   };
+  fieldsType: {
+    [key: string]: string;
+  };
+  checked: {
+    [key: string]: boolean;
+  };
+  arguments?: {
+    [key: string]: () => TAllNodes | TAllNodes;
+  };
+  parent?: TAllNodes;
+}
+
+export interface IPath {
+  fieldName: string;
+  field: IRoot;
+}
+
+export interface IInputProps {
+  updateField: (e: React.ChangeEvent<HTMLInputElement>, fieldName: string) => void;
+  fieldName: string;
+  checked: boolean;
+}
+
+export interface IPathProps {
+  path: IPath[];
+  setPath: React.Dispatch<React.SetStateAction<IPath[]>>;
 }
