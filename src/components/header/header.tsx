8373353f796a7784903df75b21b2b86a8c1dbea9
PavelPoleshchuk <97599774+PavelPoleshchuk@users.noreply.github.com>
2023-05-11 21:45:41 +0300

    feat: add authorization/authentication
    * feat: add authorization/authentication
    
    * fix: lint errors
    
    * fix: header.css

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,26 +3,66 @@ import { NavLink } from 'react-router-dom';
 import MyButton from '../myButton/myButton';
 
 import './header.scss';
+import { logout } from '../../authentication/firebase';
+import { useIsAuthorized } from '../../hooks/useIsAuthorized';
 
 export default function Header() {
   const text = lang.en;
+  const { user, navigate } = useIsAuthorized();
+  const loginOutAndGoWelcome = () => {
+    logout();
+    navigate('/');
+  };
 
   return (
     <header className="header">
-      <div className="menu">
-        <NavLink to="/" className="menu__link">
-          {text.welcome}
-        </NavLink>
-        <NavLink to="/editor" className="menu__link">
-          {text.editor}
-        </NavLink>
-        <NavLink to="/login" className="menu__link">
-          {text.login}
-        </NavLink>
-        <NavLink to="/error" className="menu__link">
-          {text.error}
-        </NavLink>
-        <MyButton content={'Sign Out'} className={'sign-out'} />
+      <div className="header__wrapper">
+        <div className="menu">
+          <NavLink to="/" className="menu__link">
+            {text.welcome}
+          </NavLink>
+          {!user && (
+            <NavLink to="/login" className="menu__link">
+              {text.login}
+            </NavLink>
+          )}
+          {!user && (
+            <NavLink to="/register" className="menu__link">
+              {text.register}
+            </NavLink>
+          )}
+          {user && (
+            <NavLink to="/editor" className="menu__link">
+              {text.main}
+            </NavLink>
+          )}
+          {/* <NavLink to="/error" className="menu__link">
+            {text.error}
+          </NavLink> */}
+        </div>
+
+        <div className="header__buttons">
+          {user && (
+            <NavLink to="/editor" className="header__btn">
+              {text.main}
+            </NavLink>
+          )}
+          {user && (
+            <button className="header__btn" onClick={loginOutAndGoWelcome}>
+              {text.logout}
+            </button>
+          )}
+          {!user && (
+            <NavLink to="/register" className="header__btn">
+              {text.register}
+            </NavLink>
+          )}
+          {!user && (
+            <NavLink to="/login" className="header__btn">
+              {text.login}
+            </NavLink>
+          )}
+        </div>
       </div>
     </header>
   );
