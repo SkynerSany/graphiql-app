1b077d8696a5e8ef22f9037d1eacb64ab6888e00
Yulia Bondar <yuliabondar98@gmail.com>
2023-05-13 18:54:13 +0300

    feat: add welcome page content

--- src/pages/welcome/welcome.tsx
+++ src/pages/welcome/welcome.tsx
@@ -12,9 +12,21 @@ export default function Welcome() {
         <h1 className="title-page">{text.title}</h1>
 
         <div className="wrapper_center">
-          <div className="d-f gap20">
-            <MyButton content={'Sign In'} />
-            <MyButton content={'Sign Up'} />
+          <div className="welcome-page__content">
+            <div className="welcome-page__content_item">
+              <h2>General information about the developers</h2>
+            </div>
+            <div className="welcome-page__content_item">
+              <h2>General information about the project</h2>
+            </div>
+            <div className="welcome-page__content_item">
+              <h2>General information about the course</h2>
+              <p>
+                The RS School is working by the principle of Pay it forward. Members of our
+                community share their knowledge and check students tasks for free. And we hope our
+                students will continue this work as our mentors in the future.
+              </p>
+            </div>
           </div>
         </div>
       </div>
