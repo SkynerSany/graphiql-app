27b394157deda6f41d9f36cbe3b3cf793623dce6
Yuliya Bondar <86925406+YuliyaBondar@users.noreply.github.com>
2023-05-13 19:16:38 +0300

    feat: welcome page content
    * feat: add welcome page content
    
    * feat: change welcome page content

--- src/pages/welcome/welcome.tsx
+++ src/pages/welcome/welcome.tsx
@@ -17,9 +17,26 @@ export default function Welcome() {
         <h1 className="title-page">{text.title}</h1>
 
         <div className="wrapper_center">
-          <div className="d-f gap20">
-            <MyButton content={'Sign In'} />
-            <MyButton content={'Sign Up'} />
+          <div className="welcome-page__content">
+            <div className="welcome-page__content_item">
+              <h2>General information about the developers</h2>
+              <p></p>
+            </div>
+            <div className="welcome-page__content_item">
+              <h2>General information about the project</h2>
+              <p>
+                GraphiQL is a playground/IDE for graphQL requests. PLEASE, READ TASK DESCRIPTION
+                CAREFULLY UP TO THE END BEFORE STARTING THE TASK.
+              </p>
+            </div>
+            <div className="welcome-page__content_item">
+              <h2>General information about the course</h2>
+              <p>
+                The RS School is working by the principle of «Pay it forward.» Members of our
+                community share their knowledge and check students&#39; tasks for free. And we hope
+                our students will continue this work as our mentors in the future.
+              </p>
+            </div>
           </div>
         </div>
       </div>
