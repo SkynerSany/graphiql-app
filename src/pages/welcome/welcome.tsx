e5c19648c6c5b063019aaf6959980fb4f52e182e
Yulia Bondar <yuliabondar98@gmail.com>
2023-05-13 19:05:36 +0300

    feat: change welcome page content

--- src/pages/welcome/welcome.tsx
+++ src/pages/welcome/welcome.tsx
@@ -15,16 +15,21 @@ export default function Welcome() {
           <div className="welcome-page__content">
             <div className="welcome-page__content_item">
               <h2>General information about the developers</h2>
+              <p></p>
             </div>
             <div className="welcome-page__content_item">
               <h2>General information about the project</h2>
+              <p>
+                GraphiQL is a playground/IDE for graphQL requests. PLEASE, READ TASK DESCRIPTION
+                CAREFULLY UP TO THE END BEFORE STARTING THE TASK.
+              </p>
             </div>
             <div className="welcome-page__content_item">
               <h2>General information about the course</h2>
               <p>
-                The RS School is working by the principle of Pay it forward. Members of our
-                community share their knowledge and check students tasks for free. And we hope our
-                students will continue this work as our mentors in the future.
+                The RS School is working by the principle of «Pay it forward.» Members of our
+                community share their knowledge and check students&#39; tasks for free. And we hope
+                our students will continue this work as our mentors in the future.
               </p>
             </div>
           </div>
